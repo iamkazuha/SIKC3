396fc95cad90d507e4e34a62e3b505878b83ef93
trisna <trisnayg02@gmail.com>
2024-12-22 11:18:03 +0700

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build' // Tambahkan ini
         }),
     ],
-    base: '/', // Pastikan base sesuai dengan lokasi aplikasi di server
-});
+    build: {
+        // Tambahkan konfigurasi build
+        outDir: 'public/build',
+        assetsDir: '',
+        manifest: true,
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js']
+        }
+    }
+})
